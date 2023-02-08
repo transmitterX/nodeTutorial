81604be7e4e1326eeaa0ad41b76da4284894d75a
Uwe Steinberg <steinberg@braunsfeld.com>
2023-02-08 15:24:48 +0100

    change cookie to secure:true

--- controllers/authController.js
+++ controllers/authController.js
@@ -38,7 +38,7 @@ const handleLogin = async (req, res) => {
     res.cookie("jwt", refreshToken, {
       httpOnly: true,
       sameSite: "None",
-      //secure: true, // important for chrome, testing with thunderclient comment!
+      secure: true, // important for chrome, testing with thunderclient comment!
       maxAge: 24 * 60 * 60 * 1000,
     })
     res.json({ accessToken })
